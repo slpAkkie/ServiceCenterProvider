918c7dcd5e5b9ce4a3953ae7271b0cc2756aef82
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-11-28 14:30:08 +0300

    Fix file opening

--- ServiceCenterProvider/Container.cs
+++ ServiceCenterProvider/Container.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace ServiceCenterProvider
@@ -19,24 +15,37 @@ namespace ServiceCenterProvider
 
             this.RequestRepository = new Repositories.RequestRepository();
             this.ConsignmentRepository = new Repositories.ConsignmentRepository();
-            using (StreamReader _StreamReader = new StreamReader("Products.csv"))
-            {
-                string Line;
-                while ((Line = _StreamReader.ReadLine()) != null)
-                {
-                    if (String.IsNullOrEmpty(Line))
-                    {
-                        continue;
-                    }
-
-                    string[] ProductData = Line.Split(';');
-                    this.ProductRepository.New(ProductData[0], ProductData[1]);
-                }
-            }
         }
 
         public void Start()
         {
+            try
+            {
+
+                using (StreamReader _StreamReader = new StreamReader("Products.csv"))
+                {
+                    string Line;
+                    while ((Line = _StreamReader.ReadLine()) != null)
+                    {
+                        if (String.IsNullOrEmpty(Line))
+                        {
+                            continue;
+                        }
+
+
+                        string[] ProductData = Line.Split(';');
+                        this.ProductRepository.New(ProductData[0], ProductData[1]);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                this.ProductRepository.New("Двигатель", "1");
+                this.ProductRepository.New("Мост", "2");
+                this.ProductRepository.New("Колеса", "3");
+                this.ProductRepository.New("Кабина", "4");
+            }
+
             Screens.MainScreen Screen = new Screens.MainScreen(this);
             Screen.Run();
         }
